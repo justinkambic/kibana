046103af97674407c220d02939db5522ffc2fe19
Justin Kambic <justin.kambic@elastic.co>
2019-02-11 11:09:01 -0500

    Add error boundary.

--- x-pack/plugins/uptime/public/uptime_app.tsx
+++ x-pack/plugins/uptime/public/uptime_app.tsx
@@ -5,6 +5,7 @@
  */
 
 import {
+  EuiErrorBoundary,
   EuiHeader,
   // @ts-ignore missing typings for EuiHeaderLink
   EuiHeaderLink,
@@ -136,119 +137,121 @@ class Application extends React.Component<UptimeAppProps, UptimeAppState> {
     const { routerBasename, graphQLClient } = this.props;
     return (
       <I18nProvider>
-        <Router basename={routerBasename}>
-          <ApolloProvider client={graphQLClient}>
-            <EuiPage className="app-wrapper-panel">
-              <EuiHeader>
-                {/*
+        <EuiErrorBoundary>
+          <Router basename={routerBasename}>
+            <ApolloProvider client={graphQLClient}>
+              <EuiPage className="app-wrapper-panel">
+                <EuiHeader>
+                  {/*
               // @ts-ignore TODO no typings for grow prop */}
-                <EuiHeaderSection grow={true}>
-                  <EuiHeaderSectionItem border="right">
-                    <EuiHeaderLogo
-                      aria-label={i18n.translate('xpack.uptime.appHeader.uptimeLogoAriaLabel', {
-                        defaultMessage: 'Go to Uptime home page',
-                      })}
-                      href="#/"
-                      iconType="heartbeatApp"
-                      iconTitle={i18n.translate('xpack.uptime.appHeader.uptimeLogoTitle', {
-                        defaultMessage: 'Uptime',
-                      })}
-                    >
-                      <FormattedMessage
-                        id="xpack.uptime.appHeader.uptimeLogoText"
-                        defaultMessage="Uptime"
-                      />
-                    </EuiHeaderLogo>
-                  </EuiHeaderSectionItem>
-                </EuiHeaderSection>
-                <EuiHeaderSection side="right">
-                  <EuiHeaderSectionItem border="none">
-                    <div
-                      style={{
-                        marginTop: '4px',
-                        marginLeft: '16px',
-                        marginRight: '16px',
-                        minWidth: '600px',
-                      }}
-                    >
-                      <EuiSuperDatePicker
-                        start={this.state.dateRangeStart}
-                        end={this.state.dateRangeEnd}
-                        isPaused={this.state.autorefreshIsPaused}
-                        refreshInterval={this.state.autorefreshInterval}
-                        onTimeChange={({ start, end }: SuperDateRangePickerRangeChangedEvent) => {
-                          this.setState(
-                            { dateRangeStart: start, dateRangeEnd: end },
-                            this.persistState
-                          );
-                        }}
-                        onRefreshChange={({
-                          isPaused,
-                          refreshInterval,
-                        }: SuperDateRangePickerRefreshChangedEvent) => {
-                          const autorefreshInterval =
-                            refreshInterval === undefined
-                              ? this.state.autorefreshInterval
-                              : refreshInterval;
-                          this.setState(
-                            { autorefreshIsPaused: isPaused, autorefreshInterval },
-                            this.persistState
-                          );
-                        }}
-                        showUpdateButton={false}
-                      />
-                    </div>
-                  </EuiHeaderSectionItem>
-                </EuiHeaderSection>
-                <EuiHeaderSection side="right">
-                  <EuiHeaderSection>
-                    <EuiHeaderLinks>
-                      <EuiHeaderLink
-                        aria-label={i18n.translate('xpack.uptime.header.helpLinkAriaLabel', {
-                          defaultMessage: 'Go to our discuss page',
+                  <EuiHeaderSection grow={true}>
+                    <EuiHeaderSectionItem border="right">
+                      <EuiHeaderLogo
+                        aria-label={i18n.translate('xpack.uptime.appHeader.uptimeLogoAriaLabel', {
+                          defaultMessage: 'Go to Uptime home page',
                         })}
-                        iconType="help"
-                        href="https://discuss.elastic.co/c/beats/heartbeat"
-                        target="_blank"
+                        href="#/"
+                        iconType="heartbeatApp"
+                        iconTitle={i18n.translate('xpack.uptime.appHeader.uptimeLogoTitle', {
+                          defaultMessage: 'Uptime',
+                        })}
                       >
                         <FormattedMessage
-                          id="xpack.uptime.header.helpLinkText"
-                          defaultMessage="Discuss"
-                          description="The link is to a support form called 'Discuss', where users can submit feedback."
+                          id="xpack.uptime.appHeader.uptimeLogoText"
+                          defaultMessage="Uptime"
                         />
-                      </EuiHeaderLink>
-                    </EuiHeaderLinks>
+                      </EuiHeaderLogo>
+                    </EuiHeaderSectionItem>
                   </EuiHeaderSection>
-                </EuiHeaderSection>
-              </EuiHeader>
-              <EuiPageContent>
-                <Switch>
-                  <Route
-                    exact
-                    path="/"
-                    render={props => (
-                      <OverviewPage
-                        {...props}
-                        {...this.state}
-                        setBreadcrumbs={this.setBreadcrumbs}
-                      />
-                    )}
-                  />
-                  <Route
-                    path="/monitor/:id"
-                    render={props => (
-                      <MonitorPage
-                        {...props}
-                        {...this.state}
-                        updateBreadcrumbs={this.setBreadcrumbs}
-                      />
-                    )}
-                  />
-                </Switch>
-              </EuiPageContent>
-            </EuiPage>
-          </ApolloProvider>
-        </Router>
+                  <EuiHeaderSection side="right">
+                    <EuiHeaderSectionItem border="none">
+                      <div
+                        style={{
+                          marginTop: '4px',
+                          marginLeft: '16px',
+                          marginRight: '16px',
+                          minWidth: '600px',
+                        }}
+                      >
+                        <EuiSuperDatePicker
+                          start={this.state.dateRangeStart}
+                          end={this.state.dateRangeEnd}
+                          isPaused={this.state.autorefreshIsPaused}
+                          refreshInterval={this.state.autorefreshInterval}
+                          onTimeChange={({ start, end }: SuperDateRangePickerRangeChangedEvent) => {
+                            this.setState(
+                              { dateRangeStart: start, dateRangeEnd: end },
+                              this.persistState
+                            );
+                          }}
+                          onRefreshChange={({
+                            isPaused,
+                            refreshInterval,
+                          }: SuperDateRangePickerRefreshChangedEvent) => {
+                            const autorefreshInterval =
+                              refreshInterval === undefined
+                                ? this.state.autorefreshInterval
+                                : refreshInterval;
+                            this.setState(
+                              { autorefreshIsPaused: isPaused, autorefreshInterval },
+                              this.persistState
+                            );
+                          }}
+                          showUpdateButton={false}
+                        />
+                      </div>
+                    </EuiHeaderSectionItem>
+                  </EuiHeaderSection>
+                  <EuiHeaderSection side="right">
+                    <EuiHeaderSection>
+                      <EuiHeaderLinks>
+                        <EuiHeaderLink
+                          aria-label={i18n.translate('xpack.uptime.header.helpLinkAriaLabel', {
+                            defaultMessage: 'Go to our discuss page',
+                          })}
+                          iconType="help"
+                          href="https://discuss.elastic.co/c/beats/heartbeat"
+                          target="_blank"
+                        >
+                          <FormattedMessage
+                            id="xpack.uptime.header.helpLinkText"
+                            defaultMessage="Discuss"
+                            description="The link is to a support form called 'Discuss', where users can submit feedback."
+                          />
+                        </EuiHeaderLink>
+                      </EuiHeaderLinks>
+                    </EuiHeaderSection>
+                  </EuiHeaderSection>
+                </EuiHeader>
+                <EuiPageContent>
+                  <Switch>
+                    <Route
+                      exact
+                      path="/"
+                      render={props => (
+                        <OverviewPage
+                          {...props}
+                          {...this.state}
+                          setBreadcrumbs={this.setBreadcrumbs}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/monitor/:id"
+                      render={props => (
+                        <MonitorPage
+                          {...props}
+                          {...this.state}
+                          updateBreadcrumbs={this.setBreadcrumbs}
+                        />
+                      )}
+                    />
+                  </Switch>
+                </EuiPageContent>
+              </EuiPage>
+            </ApolloProvider>
+          </Router>
+        </EuiErrorBoundary>
       </I18nProvider>
     );
   }
