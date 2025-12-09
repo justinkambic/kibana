b71a3035931ca9240750ad129181565fa582a91f
Justin Kambic <jk@elastic.co>
2025-12-09 15:49:51 -0500

    Fix edit default rules popover.

--- x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/alerts/hooks/use_synthetics_rules.tsx
+++ x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/alerts/hooks/use_synthetics_rules.tsx
@@ -16,6 +16,7 @@ import { useSyntheticsSettingsContext } from '../../../contexts';
 import {
   selectSyntheticsAlerts,
   selectSyntheticsAlertsLoading,
+  selectSyntheticsAlertsLoaded,
 } from '../../../state/alert_rules/selectors';
 import {
   enableDefaultAlertingSilentlyAction,
@@ -35,6 +36,7 @@ export const useSyntheticsRules = (isOpen: boolean) => {
 
   const defaultRules = useSelector(selectSyntheticsAlerts);
   const loading = useSelector(selectSyntheticsAlertsLoading);
+  const rulesLoaded = useSelector(selectSyntheticsAlertsLoaded);
   const alertFlyoutVisible = useSelector(selectAlertFlyoutVisibility);
   const isNewRule = useSelector(selectIsNewRule);
   const { settings } = useSelector(selectDynamicSettings);
@@ -69,6 +71,13 @@ export const useSyntheticsRules = (isOpen: boolean) => {
     // we don't want to run this on defaultRules change
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, isOpen, hasMonitors, defaultRulesEnabled]);
+
+  // Fetch default rules when popover opens if they haven't been loaded yet
+  useEffect(() => {
+    if (isOpen && !loading && rulesLoaded === null) {
+      dispatch(getDefaultAlertingAction.get());
+    }
+  }, [isOpen, loading, rulesLoaded, dispatch]);
 
   const {
     triggersActionsUi: { ruleTypeRegistry, actionTypeRegistry },
